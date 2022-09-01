8a15370c5a87b7fd13b5d9144c9b81fd5172eff9
pengchihui1 <pengchihui1@gmail.com>
2022-09-02 00:13:27 +0800

    调整

---
+++ component/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 全局不知 -->
+  <Layout />
+</template>
+<script>
+import Loading from "./Loading.vue";
+import Layout from "./Layout.vue";
+export default {
+  name: "Home",
+  data() {
+    return {
+      isLogouting: false,
+    };
+  },
+  components: {
+    Loading,
+    Layout,
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.info {
+  background: #fff;
+  width: 100%;
+  height: 600px;
+  color: #2c3e50;
+  text-align: center;
+  padding-top: 170px;
+}
+</style>
